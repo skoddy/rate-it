d00c0fee9a8c114dea0779854f863a0ff463d940
skodowski <m.t.skodowski@gmail.com>
2018-10-05 18:00:34 +0200

    prod. enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,18 @@
 export const environment = {
-  production: true
+  appName: 'RATE-IT',
+  envName: 'PROD',
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB5uwLUmV7G4-06l1t06SOOY5IZDtDLvhk',
+    authDomain: 'ang-fir-mat.firebaseapp.com',
+    databaseURL: 'https://ang-fir-mat.firebaseio.com',
+    projectId: 'ang-fir-mat',
+    storageBucket: 'ang-fir-mat.appspot.com',
+    messagingSenderId: '776756120719'
+  },
+  admin: {
+    apiKey: 'AIzaSyB5uwLUmV7G4-06l1t06SOOY5IZDtDLvhk',
+    authDomain: 'ang-fir-mat.firebaseapp.com',
+    databaseURL: 'https://ang-fir-mat.firebaseio.com'
+  }
 };

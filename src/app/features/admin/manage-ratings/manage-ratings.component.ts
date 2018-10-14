7f9bf9a3f1ec395bbf1316fc573e5cc0a295314d
skodowski <m.t.skodowski@gmail.com>
2018-10-14 11:42:48 +0200

    animation

--- src/app/features/admin/manage-ratings/manage-ratings.component.ts
+++ src/app/features/admin/manage-ratings/manage-ratings.component.ts
@@ -12,39 +12,20 @@ import { trigger, state, style, transition, animate, keyframes, query, stagger }
   templateUrl: './manage-ratings.component.html',
   styleUrls: ['../admin.component.css'],
   animations: [
-
-    trigger('listAnimation', [
+    trigger('flyInOut', [
       transition('* => *', [
-
-        query(':enter', style({ opacity: 0 }), { optional: true }),
-
-        query(':enter', stagger('300ms', [
-          animate('1s ease-in', keyframes([
-            style({ opacity: 0, transform: 'translateY(-75%)', offset: 0 }),
-            style({ opacity: .5, transform: 'translateY(35px)', offset: 0.3 }),
-            style({ opacity: 1, transform: 'translateY(0)', offset: 1.0 }),
-          ]))]), { optional: true }),
-
-        query(':leave', stagger('300ms', [
-          animate('1s ease-in', keyframes([
-            style({ opacity: 1, transform: 'translateY(0)', offset: 0 }),
-            style({ opacity: .5, transform: 'translateY(35px)', offset: 0.3 }),
-            style({ opacity: 0, transform: 'translateY(-75%)', offset: 1.0 }),
-          ]))]), { optional: true })
-      ])
-    ]),
-    trigger('explainerAnimation', [
-      transition('* => *', [
-        query('.col', style({ opacity: 0, transform: 'translateX(-40px)' })),
-
-        query('.col', stagger('500ms', [
-          animate('800ms 1.2s ease-out', style({ opacity: 1, transform: 'translateX(0)' })),
-        ])),
-
-        query('.col', [
-          animate(1000, style('*'))
-        ])
-
+        animate(300, keyframes([
+          style({opacity: 0, offset: 0}),
+          style({opacity: .5, offset: 0.3}),
+          style({opacity: 1, offset: 1.0})
+        ]))
+      ]),
+      transition('* => void', [
+        animate(300, keyframes([
+          style({ opacity: 1, transform: 'translateX(0)', offset: 0 }),
+          style({ opacity: 1, transform: 'translateX(-15px)', offset: 0.7 }),
+          style({ opacity: 0, transform: 'translateX(100%)', offset: 1.0 })
+        ]))
       ])
     ])
   ],
